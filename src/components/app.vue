c1bde5961f6efd9a03a0d1cf888cfdc34391eb25
Jonnie Spratley <jonnie.spratley@ge>
2017-12-26 15:36:06 -0800

    added example for vue

---
+++ src/components/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="appView">
+    <h1>{{framework}} Micro-app</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        framework: 'Vue.js',
+				items: [
+					'Learn Vue.js',
+					'Create micro-app',
+					'Deploy'
+				]
+      }
+    }
+  }
+</script>
+
+<style lang='scss' scoped>
+  h1 {
+    font-size: 2rem;
+    color: green;
+  }
+</style>
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('index.js');
+import Vue from 'vue/dist/vue.js';
+import App from './components/app.vue';
+import './style';
+
+const app = window.addEventListener('load', ()=>{
+	new Vue({
+	  el: '#vueApp',
+		name: App,
+		components: { App },
+		template: `<App/>`,
+	  data: {
+	    todos: [
+	      { text: 'Learn JavaScript' },
+	      { text: 'Learn Vue' },
+	      { text: 'Build something awesome' }
+	    ]
+	  }
+	})
+})
+export default app;
